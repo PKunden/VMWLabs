27f0eb267eb73b6e99c2a17e58f055cd556611de
preetam <preetam.vmw@gmail.com>
2023-06-10 10:45:08 +0200

    just formatting changed

--- Prepare_Windows/Installwintools.ps1
+++ Prepare_Windows/Installwintools.ps1
@@ -7,7 +7,6 @@ Install-WindowsFeature RSAT
 
 
 # install PowerShell Module for VMware Cloud Foundation Reporting
-
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0
 Install-Module -Name VMware.vSphere.SsoAdmin -MinimumVersion 1.3.9
