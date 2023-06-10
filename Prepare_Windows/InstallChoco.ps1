8b0a12648c86abc23440be18eb0aa7fd5cac6c24
preetam <preetam.vmw@gmail.com>
2023-06-10 10:43:55 +0200

    added comment for reboot might required

--- Prepare_Windows/InstallChoco.ps1
+++ Prepare_Windows/InstallChoco.ps1
@@ -1,2 +1,3 @@
 Set-ExecutionPolicy AllSigned
+# after executing the script, you might have to reboot the machine and execute chocolate
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
