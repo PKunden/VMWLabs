9f22ebddf7e7964977d65c548597845557be7310
preetam <preetam.vmw@gmail.com>
2023-06-17 11:56:35 +0200

    added allow clobber

--- Prepare_Windows/Installwintools.ps1
+++ Prepare_Windows/Installwintools.ps1
@@ -7,9 +7,10 @@ Install-WindowsFeature RSAT
 
 
 # install PowerShell Module for VMware Cloud Foundation Reporting
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0
-Install-Module -Name VMware.vSphere.SsoAdmin -MinimumVersion 1.3.9
+Install-Module -Name VMware.vSphere.SsoAdmin -MinimumVersion 1.3.9 -AllowClobber
 Install-Module -Name PowerVCF -MinimumVersion 2.3.0
 Install-Module -Name PowerValidatedSolutions -MinimumVersion 2.2.0
 Install-Module -Name VMware.CloudFoundation.Reporting
