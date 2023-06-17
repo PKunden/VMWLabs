12602e75aa34872aa097aef0bc5258f1b4c652c3
preetam <preetam.vmw@gmail.com>
2023-06-17 11:56:48 +0200

    modified the command

--- Prepare_Windows/getdccert.sh
+++ Prepare_Windows/getdccert.sh
@@ -1,1 +1,6 @@
 openssl.exe s_client nexus.corp.local:636
+# from linux try below command
+openssl s_client -connect nexus.corp.local:636 | openssl x509
+
+# Inputs for SSO
+ldaps://nexus.corp.local:636
