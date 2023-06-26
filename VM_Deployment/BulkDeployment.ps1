1eb38db09fdc667861628ba0b0245364e317d610
preetam <preetam.vmw@gmail.com>
2023-06-26 21:23:17 +0200

    comments were added in the script

--- VM_Deployment/BulkDeployment.ps1
+++ VM_Deployment/BulkDeployment.ps1
@@ -15,26 +15,28 @@ $ip = "192.168.73.2$($i+5)"
 # just to check vm name and ip details
 Write-Host $vmname 
 write-host $ip
-$vmlocation = "AZ1-VMs"
-$datastorename = "vsanDatastore"
-$specname = "ubuntu"
-$subnetmask = "255.255.255.0"
-$gateway = "192.168.73.254"
-$vmhostname = "esx-01a.corp.internal"
-$templatename = "lu22-t"
-$vsanpolicyName = "Policy-SFTT1"
-$template = get-template -Name $templatename
-$specs = Get-OSCustomizationSpec -Name $specname
-$datastore = get-datastore -Name $datastorename
-$vsansp = Get-SpbmStoragePolicy -Name $vsanpolicyName
-Get-OSCustomizationSpec $specs | Get-OSCustomizationNicMapping | Set-OSCustomizationNicMapping –IpMode UseStaticIP –IpAddress $ip –SubnetMask $subnetmask –DefaultGateway $gateway
-New-VM -Name $vmname -Template $template -Datastore $datastore -VMHost (get-vmhost $vmhostname) -OSCustomizationSpec $specs -Location (Get-Folder -Name $vmlocation) -StoragePolicy $vsansp
-write-host "Waiting for 10 seconds"
-Start-Sleep -Seconds 10
-# here you can change VM settings e.g. Portgroup, Memory and CPU
-Get-VM $vmname | Get-NetworkAdapter | Set-NetworkAdapter -StartConnected $true -Confirm:$false -OutVariable $null
-get-vm $vmname | Set-VM -MemoryGB 4 -Confirm:$false
-Start-Sleep 10
-start-vm $vmname -Confirm:$false -RunAsync -OutVariable $null
+    $vmlocation = "AZ1-VMs" # folder where you to locate VM
+    $datastorename = "vsanDatastore" # datastore name.
+    $specname = "ubuntu" # name of the specification you have created.
+    $subnetmask = "255.255.255.0" #
+    $gateway = "192.168.73.254"
+    $vmhostname = "esx-01a.corp.internal" # selecting host here instead of cluster
+    $templatename = "lu22-t" # this is the template name
+    $vsanpolicyName = "Policy-SFTT1" # configure vSAN policy as per the requirements
+
+    $template = get-template -Name $templatename
+    $specs = Get-OSCustomizationSpec -Name $specname
+    $datastore = get-datastore -Name $datastorename
+    $vsansp = Get-SpbmStoragePolicy -Name $vsanpolicyName
+    Get-OSCustomizationSpec $specs | Get-OSCustomizationNicMapping | Set-OSCustomizationNicMapping –IpMode UseStaticIP –IpAddress $ip –SubnetMask $subnetmask –DefaultGateway $gateway
+    New-VM -Name $vmname -Template $template -Datastore $datastore -VMHost (get-vmhost $vmhostname) -OSCustomizationSpec $specs -Location (Get-Folder -Name $vmlocation) -StoragePolicy $vsansp
+    write-host "Waiting for 10 seconds"
+    Start-Sleep -Seconds 10
+
+    # here you can change VM settings e.g. Portgroup, Memory and CPU
+    Get-VM $vmname | Get-NetworkAdapter | Set-NetworkAdapter -StartConnected $true -Confirm:$false -OutVariable $null # i have ensure that vNIC is connected. sometimes it is not connected
+    get-vm $vmname | Set-VM -MemoryGB 4 -Confirm:$false
+    Start-Sleep 10 # little pause
+    start-vm $vmname -Confirm:$false -RunAsync -OutVariable $null # start VM
 Start-Sleep 10
 }
