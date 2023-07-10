0645c7fe55afed294e90d3115cd91e848b51abc8
preetam <preetam.vmw@gmail.com>
2023-07-10 12:40:58 +0200

    Change the code to use array

--- AriaSuite/vIDM/createou.ps1
+++ AriaSuite/vIDM/createou.ps1
@@ -1,7 +1,34 @@
-$baseGroupOU = "Security Groups"
-$baseUserOU = "Security Users"
+# Create OU before you start with anything.
+# in production you may not have to do this.
 
+$DC1 = "corp" # change this
+$DC2 = "local" # change this
+$OUs = @() # array declared
+$OUs = ("Sales Groups", "Sales Users") # change this
+$ADPath = "DC=" + $DC1 + "," + "DC=" + $DC2 # path is built e.g. DC=corp,DC=local
+
+# $ADPath
 #create a OU
-New-ADOrganizationalUnit -name $baseGroupOU -Path "DC=corp,DC=local"
-New-ADOrganizationalUnit -name $baseUserOU -Path "DC=corp,DC=local"
+foreach ($OU in $OUs) {
+    New-ADOrganizationalUnit -name $OU -Path $ADPath
+}
 
+# check if OU are created
+foreach ($OU in $OUs) {
+    # New-ADOrganizationalUnit -name $OU -Path $ADPath
+    $OUName = "OU=" + $OU + "," + $ADPath # create a path
+    Get-ADOrganizationalUnit -Identity $OUName
+}
+
+######################################## ignore below#################################################
+
+<#  
+ $baseGroupOU = "Sales Groups"
+ $baseUserOU = "Sales Users"
+ Get-ADOrganizationalUnit -Identity "OU=ICT Users,DC=corp,DC=local"
+ $OUName = "OU=" + $baseUserOU + "," + $ADPath
+ Get-ADOrganizationalUnit -Identity $OUName 
+ Get-ADOrganizationalUnit -Identity "OU=" + $baseUserOU + "," + $ADPath
+ New-ADOrganizationalUnit -name $baseGroupOU -Path $ADPath
+ New-ADOrganizationalUnit -name $baseUserOU -Path $ADPath 
+ #>
