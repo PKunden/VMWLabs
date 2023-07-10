737a07cb65bf20076cdd07b2070121c7af907f0d
preetam <preetam.vmw@gmail.com>
2023-07-10 09:15:52 +0200

    New file for adding vc credential

---
+++ prepare_powercli.ps1
@@ -0,0 +1,13 @@
+# This scripts add a credential to vi credential store.
+# highly recommended for lab configuration and first time usage.
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+$ADDomain = "corp.local" # change this one
+$VCENTERHOSTNAME = "vc01" # change this one
+$VCFQDN = $VCENTERHOSTNAME + '.' + $ADDomain
+$ADMINUSER = "administrator@vsphere.local" # almost never changes in lab
+$ADMINPASS = "PSGlobalCloud123!" # change this one
+
+############################ DO NOT CHANGE ANYTHING AFTER THIS ######################################
+New-VICredentialStoreItem -Host $VCFQDN -User $ADMINUSER  -Password $ADMINPASS
+# use -force for self signed certificate
+Connect-VIServer $VCFQDN -Force
