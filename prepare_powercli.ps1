edff4da0a7f4239c0649e2dbda5097f5e3ceaf22
preetam <preetam.vmw@gmail.com>
2023-08-23 19:25:11 +0200

    little changes

--- prepare_powercli.ps1
+++ prepare_powercli.ps1
@@ -1,6 +1,6 @@
 # This scripts add a credential to vi credential store.
 # highly recommended for lab configuration and first time usage.
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true -confirm:$false
 $ADDomain = "corp.local" # change this one
 $VCENTERHOSTNAME = "vc01" # change this one
 $VCFQDN = $VCENTERHOSTNAME + '.' + $ADDomain
